6c7c262ff9ca28b997845b3c9be9bf20fd9d7895
Murilo <alcantumurilo@gmail.com>
2025-07-07 23:48:32 -0400

    Finish configuring target model for sqlalchey

--- app/models/target.py
+++ app/models/target.py
@@ -1,12 +1,18 @@
 from sqlalchemy import Column, Integer, String, Text, JSON
 from sqlalchemy.orm import relationship
+from db.base import Base
 
-class Target:
-    name: str | None
-    email: str | None
-    phone: str | None
-    domain: list[str] | None
-    usernames: list[str] | None
-    notes: str | None
-    cnpj: str | None
-    cpf: str | None
+class Target(Base):
+    __tablename__ = 'target'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nulable=True)
+    email = Column(String(150), nullable=True, unique=False)
+    phone = Column(String(20), nullable=True)
+    domains = Column(JSON, nullable=True)
+    usernames = Column(JSON, nullable=True)
+    notes = Column(Text, nullable=True)
+    cnpj = Column(String(14), nullable=True)
+    cpf = Column(String(11), nullable=True)
+
+    results = relationship("SearchResults", back_populates="targets")
