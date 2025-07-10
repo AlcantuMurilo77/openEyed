7ae922fa03a5fd69a39e346188cdda623b767e93
Murilo <alcantumurilo@gmail.com>
2025-07-09 23:29:01 -0400

    Build DataSource model

--- app/models/data_source.py
+++ app/models/data_source.py
@@ -1,6 +1,15 @@
+from sqlalchemy import Column, Integer, String, Text, JSON
+from sqlalchemy.orm import relationship
+from db.base import Base
 
 
 class DataSource:
+    __tablename__ = 'data_source'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), nullable=False)
+    type = Column(String(50), nullable=False)
+    base_url = Column(String(100), nullable=False)
+
     name: str
     type: str
     base_url: str
